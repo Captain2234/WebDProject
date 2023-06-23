d450474c00a446aaf988d6fbbf5684d474cb95bf
Bhavishya <106080668+Captain2234@users.noreply.github.com>
2023-06-23 15:05:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -45,8 +45,7 @@ function email_Verify() {
   else{
     email.style.border = "1px solid red";
     email_error.style.display = "block";
-    alert()
-  }
+      }
 }
 function pass_Verify() {
   if (password.value.length >= 5) {
